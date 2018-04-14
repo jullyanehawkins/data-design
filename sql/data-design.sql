184076a1c57f33abb3a9e6a3a51593ec91998f2a
Jullyane Hawkins <jullyanehawkins@gmail.com>
2018-04-13 21:59:12 -0600

    added ddl script

---
+++ sql/data-design.sql
@@ -0,0 +1,37 @@
+ALTER DATABASE jhawkins20 CHARACTER SET utf8 COLLATE utf8_unicode_ci;
+
+DROP TABLE IF EXISTS `ratings`;
+DROP TABLE IF EXISTS movie;
+DROP TABLE IF EXISTS profile;
+
+CREATE TABLE profile (
+	profileId BINARY(16) NOT NULL,
+	profileActivationToken CHAR(32),
+	profileAtHandle VARCHAR(32) NOT NULL,
+	profileEmail VARCHAR(128) NOT NULL,
+	profileHash	CHAR(128) NOT NULL,
+	profileName VARCHAR(32),
+	UNIQUE(profileAtHandle),
+	UNIQUE(profileEmail),
+	PRIMARY KEY(profileId)
+);
+
+CREATE TABLE movie (
+	movieId BINARY(16) NOT NULL,
+	movieName VARCHAR(32),
+	movieDirector VARCHAR(32),
+	movieGenre VARCHAR(32),
+	movieLength TINYINT(1),
+	UNIQUE(movieId),
+	PRIMARY KEY(movieId)
+);
+
+CREATE TABLE `ratings` (
+	ratingsProfileId BINARY(16) NOT NULL,
+	ratingsMovieId BINARY(16) NOT NULL,
+	INDEX(ratingsProfileId),
+	INDEX(ratingsMovieId),
+	FOREIGN KEY(ratingsProfileId) REFERENCES profile(profileId),
+	FOREIGN KEY(ratingsMovieId) REFERENCES movie(movieId),
+	PRIMARY KEY(ratingsProfileId, ratingsMovieId)
+);
