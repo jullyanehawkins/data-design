8138d02331671231bf64468af949e12e52465b8f
Jullyane Hawkins <jullyanehawkins@gmail.com>
2018-04-13 23:01:34 -0600

    added an unordered list

--- index.php
+++ index.php
@@ -5,8 +5,8 @@
 		<title>Data Design</title>
 	</head>
 	<body>
-		<h1><em>Data Design</em></h1>
-		<div class="persona"><h2>Persona</h2>
+		<h1>Data Design</h1>
+		<div><h2>Persona</h2>
 			<p>Jully is an almost 30 years old woman 👵🏻 who loves movies. Because of her OCD, she enjoys rating the movies that she watched. One way that she finds to do so is by using the website https://filmow.com/ with her MacBook (macOS High Sierra 10.13.4) and on her iPhone X (iOS 11.3). As a Millennial, she is very confident with technology.</p>
 		</div>
 		<div>
@@ -14,6 +14,18 @@
 			<p>As a user, Jully wants to rate movies on Filmow.</p>
 		</div>
 		<img src = "data%20design%20website.JPG">
-		<img src = "data%20design%20website.JPG">
+		<div>
+			<h2>Use Case/Interaction Flow</h2>
+			<p>Precondition: Jully is logged in her account.</p>
+			<ul>
+				<li>Jully is on her profile page</li>
+				<li>She types a movie name in the search bar and clicks ok</li>
+				<li>Site shows the possible results for her search</li>
+				<li>Jully clicks on the right movie link</li>
+				<li>Site shows the movie page</li>
+				<li>Jully clicks on the star button to rate the movie</li>
+				<li>Site saves her rating and show it to her</li>
+			</ul>
+		<img src = "data%20design%20erd.JPG">
 	</body>
 </html>
