52584f770a17465d571bcf638866f0a30c10f3eb
Jullyane Hawkins <jullyanehawkins@gmail.com>
2018-04-13 22:52:07 -0600

    added images, but need to resize them

--- index.php
+++ index.php
@@ -5,6 +5,15 @@
 		<title>Data Design</title>
 	</head>
 	<body>
-
+		<h1><em>Data Design</em></h1>
+		<div class="persona"><h2>Persona</h2>
+			<p>Jully is an almost 30 years old woman 👵🏻 who loves movies. Because of her OCD, she enjoys rating the movies that she watched. One way that she finds to do so is by using the website https://filmow.com/ with her MacBook (macOS High Sierra 10.13.4) and on her iPhone X (iOS 11.3). As a Millennial, she is very confident with technology.</p>
+		</div>
+		<div>
+			<h2>User Story</h2>
+			<p>As a user, Jully wants to rate movies on Filmow.</p>
+		</div>
+		<img src = "data%20design%20website.JPG">
+		<img src = "data%20design%20website.JPG">
 	</body>
 </html>
