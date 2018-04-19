4326afe01dd07af134e56042d8d302b0053a7213
Jullyane Hawkins <jullyanehawkins@gmail.com>
2018-04-19 13:56:03 -0600

    added doc blocks to the methods

--- php/classes/Profile.php
+++ php/classes/Profile.php
@@ -33,51 +33,97 @@ class Profile {
 	 * @var string $profileName //?
 	 **/
 	private $profileName;
-
+	/**
+	 * accessor method for getting profileId
+	 *
+	 * @return Uuid value for profileId (or null if new profile)
+	 **/
 	public function getProfileId() {
 		return $this->profileId;
 	}
-
+	/**
+	 * mutator method for profileId
+	 *
+	 * @param Uuid|string $newProfileId with the value of profileId
+	 * @throws \RangeException if $newProfileId is not positive
+	 * @throws \TypeError id profile id is not positive
+	 **/
 	public function setProfileId($profileId): void {
 		$this->profileId = $profileId;
 	}
-
+	/**
+	* accessor method for account activation token
+	*
+	* @return string value of the activation token
+	**/
 	public function getProfileActivationToken() {
 		return $this->profileActivationToken;
 	}
-
+	/**
+	 * mutator method for account activation token
+	 *
+	 * @param string $newProfileActivationToken
+	 * @throws \InvalidArgumentException  if the token is not a string or insecure
+	 * @throws \RangeException if the token is not exactly 32 characters
+	 * @throws \TypeError if the activation token is not a string
+	 **/
 	public function setProfileActivationToken($profileActivationToken): void {
 		$this->profileActivationToken = $profileActivationToken;
 	}
-
-	public function getProfileAtHandle() {
-		return $this->profileAtHandle;
-	}
-
-	public function setProfileAtHandle($profileAtHandle): void {
-		$this->profileAtHandle = $profileAtHandle;
-	}
-
+	/**
+	 * accessor method for profile email
+	 *
+	 * @return string value of profile email
+	 **/
 	public function getProfileEmail() {
 		return $this->profileEmail;
 	}
-
+	/**
+	 * mutator method for profile email
+	 *
+	 * @param string $newProfileEmail new value of profile email
+	 * @throws \InvalidArgumentException if $newEmail is not a valid profile email or insecure
+	 * @throws \RangeException if $newProfileEmail is > 128 characters
+	 * @throws \TypeError if $newProfileEmail is not a string
+	 **/
 	public function setProfileEmail($profileEmail): void {
 		$this->profileEmail = $profileEmail;
 	}
-
+	/**
+	 * accessor method for profileHash
+	 *
+	 * @return string value of profile hash
+	 **/
 	public function getProfileHash() {
 		return $this->profileHash;
 	}
-
+	/**
+	 * mutator method for profile hash password
+	 *
+	 * @param string $newProfileHash
+	 * @throws \InvalidArgumentException if the hash is not secure
+	 * @throws \RangeException if the hash is not 128 characters
+	 * @throws \TypeError if profile hash is not a string
+	 **/
 	public function setProfileHash($profileHash): void {
 		$this->profileHash = $profileHash;
 	}
-
+	/**
+	 * accessor method for profile name
+	 *
+	 * @return string value
+	 **/
 	public function getProfileName() {
 		return $this->profileName;
 	}
-
+	/**
+	 * mutator method for profile name
+	 *
+	 * @param string $newProfileName new value of name
+	 * @throws \InvalidArgumentException if $newName is not a string or insecure
+	 * @throws \RangeException if $newName is > 32 characters
+	 * @throws \TypeError if $newName is not a string
+	 **/
 	public function setProfileName($profileName): void {
 		$this->profileName = $profileName;
 	}
