8ce6ecdc7204290fe1ec7f026bd3fb5c345afd60
Jullyane Hawkins <jullyanehawkins@gmail.com>
2018-04-23 16:13:20 -0600

    inserted, updated and deleted data from tables

---
+++ sql/oml-assignment.sql
@@ -0,0 +1,19 @@
+INSERT INTO profile(profileId,profileActivationToken, profileEmail, profileHash, profileName)
+VALUES(UNHEX(REPLACE("f6d46fac-c4a0-49c6-bc70-fb0fbeeacb6b", "-", "")), "012345", "jullyanejgf@gmail.com", "67890", "jully");
+
+UPDATE profile
+SET profileName = "Jullyane"
+WHERE profileName = "jully;"
+
+DELETE FROM profile
+WHERE profileEmail = "jullyanejgf@gmail.com";
+
+INSERT INTO movie(movieId, movieName, movieDirector, movieGenre, movieLength)
+VALUES(UNHEX(REPLACE("690319b4-1111-48f1-a3c8-0003198c94db", "-", "")), "Reservoir Dogs", "Quentin Tarantino", "Thriller", "1h39m");
+
+UPDATE movie
+SET movieGenre = "Crime"
+WHERE movieGenre = "Thriller";
+
+DELETE FROM movie
+WHERE movieId = UNHEX(REPLACE("690319b4-1111-48f1-a3c8-0003198c94db", "-", "");
