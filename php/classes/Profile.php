bc6099742d2ea8b53bfc2b99abce013fe8a8060f
Jullyane Hawkins <jullyanehawkins@gmail.com>
2018-04-23 19:56:41 -0600

    edited class profile

--- php/classes/Profile.php
+++ php/classes/Profile.php
@@ -1,13 +1,17 @@
 <?php
+namespace Edu\Cnm\jhawkins20\DataDesign;
+require_once("autoload.php");
+require_once(dirname(__DIR__) . "../vendor/autoload.php");
+use Edu\Cnm\DataDesign\ValidateUuid;
+use Ramsey\Uuid\Uuid;
 /**
  * Small Cross Section of a "Filmow" Profile
  *
  * This is a cross section of what is stored in the database of a profile on filmow.com.
  *
  * @author Jullyane Hawkins <jhawkins20@cnm.edu>
- * @version 1.0.0
+ * @version 2.0.0
  **/
-
 class Profile implements \JsonSerializable {
 	use \Edu\Cnm\DataDesign\ValidateUuid;
 	/**
@@ -34,6 +38,37 @@ class Profile implements \JsonSerializable {
 	 * @var string $profileName
 	 **/
 	private $profileName;
+
+	/**
+	 * constructor for this Profile
+	 *
+	 * @param string|Uuid $newProfileId id of this Profile or null if a new Profile
+	 * @param int $newProfileActivationToken integer verifying the profile user
+	 * @param string $newProfileEmail string of user's email address
+	 * @param $newProfileHash
+	 * @param string $newProfileName string of the user's profile name
+	 *
+	 * @throws \InvalidArgumentException if data types are not valid
+	 * @throws \RangeException if data values are out of bounds (e.g., strings too long, negative integers)
+	 * @throws \TypeError if data types violate type hints
+	 * @throws \Exception if some other exception occurs
+	 * @Documentation https://php.net/manual/en/language.oop5.decon.php
+	 **/
+	public function __construct($newProfileId, $newProfileActivationToken, string $newProfileEmail, $newProfileHash, $newProfileName) {
+		try {
+			$this->setProfileId($newProfileId);
+			$this->setProfileActivationToken($newProfileActivationToken);
+			$this->setProfileEmail($newProfileEmail);
+			$this->setProfileHash($newProfileHash);
+			$this->setProfileName($newProfileName);
+		}
+			//determine what exception type was thrown. List in priority order.
+		catch(\InvalidArgumentException | \RangeException | \Exception | \TypeError $exception) {
+			$exceptionType = get_class($exception);
+			//rethrow the exception. Constructors are not the place to take action on exceptions, we want to "pass the buck."
+			throw(new $exceptionType($exception->getMessage(), 0, $exception));
+		}
+	}
 	/**
 	 * accessor method for profileId
 	 *
@@ -189,4 +224,142 @@ class Profile implements \JsonSerializable {
 		// store the name
 		$this->profileName = $newProfileName;
 	}
+}
+/**
+ * inserts the entire class of "profile" into mySQL
+ *
+ * @param \PDO $pdo PDO live connection to the database
+ * @throws \PDOException when mySQL related error occurs. PDO's own label of exception.
+ * @throws \TypeError if $pdo is not a PDO connection object
+ **/
+public function insert(\PDO $pdo) : void {
+	//creates query template
+	$query = "INSERT INTO profile(profileId, profileActivationToken, profileEmail, profileName) VALUES (:profileId, :profileActivationToken, :profileEmail, :profileName)";
+	$statement = $pdo->prepare($query);
+	//bind the member variables to the place holders in the template
+	$parameters = ["profileId" => $this->profileId->getBytes(), "profileActivationToken" => $this->profileActivationToken->getBytes(), "profileEmail" => $this->profileEmail, "profileName" => $this->profileName];
+	$statement->execute($parameters);
+}
+/**
+ * deletes this profileId row from mySQL
+ *
+ * @param \PDO $pdo PDO connection object
+ *
+ * @throws \PDOException when mySQL related errors occur
+ * @throws \TypeError if $pdo is not a PDO connection object
+ **/
+public function delete(\PDO $pdo) : void {
+	// creates a query template
+	$query = "DELETE FROM profile WHERE profileId = :profileId";
+	$statement = $pdo->prepare($query);
+	// bind the member variables to the place holder in the template.
+	$parameters = ["profileId" => $this->profileId->getBytes()];
+	$statement->execute($parameters);
+}
+/**
+ * updates this profile in mySQL
+ *
+ * @param \PDO $pdo PDO connection object
+ *
+ * @throws \PDOException when mySQL related errors occur
+ * @thows \TypeError if $pdo is not a PDO connection object
+ **/
+public function update(\PDO $pdo): void {
+	//create query template
+	$query = "UPDATE profile SET profileActivationToken = :profileActivationToken, profileEmail = :profileEmail, profileName = :profileName WHERE profileId = :profileId";
+	$statement = $pdo->prepare($query);
+	$parameters = ["profileId" => $this->profileId->getBytes(), "profileActivationToken" => $this->profileProfileId->getBytes(), "profileEmail" => $this->profileEmail, "profileName" => $this->profileName];
+	$statement->execute($parameters);
+}
+/**
+ * gets the profile by profileId
+ *
+ * @param \PDO $pdo PDO connection object
+ * @param Uuid|string $profileId profile id to search for
+ *
+ * @return Profile|null profile found or null if not found
+ *
+ * @throws \PDOException when mySQL related error occur
+ * @throws \TypeError when a variable are not the correct data type
+ **/
+public static function getProfileByProfileId(\PDO $pdo, $profileId) : ?Profile {
+	// sanitize the profileId before searching
+	try {
+		$profileId = self::validateUuid($profileId);
+	}
+	catch (\InvalidArgumentException | \RangeException | \Exception | \TypeError $exception)
+	{
+		throw(new \PDOException($exception->getMessage(), 0, $exception));
+	}
+	//create query template
+	$query = "SELECT profileId, profileActivationToken, profileEmail, profileName FROM profile WHERE profileId = :profileId";
+	$statement = $pdo->prepare($query);
+	//bind the profile id to the placeholder in the template
+	$parameters = ["profileId" => $profileId->getBytes()];
+	$statement->execute($parameters);
+	//grab the profile from mySQL
+	try {
+		$profile = null;
+		//give results in the "associative array" format; grabs results one row at a time and returns false when done.
+		$statement->setFetchMode(\PDO::FETCH_ASSOC);
+		$row = $statement->fetch();
+		if ($row !== false) {
+			$profile = new Profile ($row["profileId"], $row["profileActivationToken"], $row["profileEmail"], $row["profileName"]);
+		}
+	}
+		//a fail safe to catch any further issues
+	catch(\Exception $exception) {
+		//if the row couldn't be converted, re-throw it
+		throw(new \PDOException($exception->getMessage(), 0, $exception));
+	}
+	return($profile);
+}
+/**
+ * gets all profiles
+ *
+ * @param \PDO $pdo PDO connection object
+ *
+ * @return \SplFixedArray SplFixedArray of profiles found or null if not found
+ *
+ * @throws \PDOException when mySQL related errors occur
+ * @throws \TypeError when variables are not the correct data type
+ **/
+public static function getAllProfiles(\PDO $pdo) : \SPLFixedArray {
+	//create a query template
+	$query = "SELECT profileId, profileActivationToken, profileEmail, profileName FROM profile";
+	$statement = $pdo->prepare($query);
+	$statement->execute();
+	//builds an array of profiles
+	$profiles = new \SplFixedArray($statement->rowCount());
+	$statement->setFetchMode(\PDO::FETCH_ASSOC);
+	while(($row = $statement->fetch()) !== false) {
+		try {
+			$profile = new Profile ($row ["profileId"], $row["profileActivationToken"], $row["profileEmail"], $row["profileName"]);
+			$profiles[$profiles->key()] = $profile;
+			$profiles->next();
+		}
+		catch(\Exception $exception){
+			//if the row couldn't be converted, re-throw it
+			throw(new \PDOException($exception->getMessage(), 0, $exception));
+		}
+	}
+	return ($profiles);
+}
+/**
+ * formats the state variables for JSON serialization
+ *
+ * @return array resulting state variables to serialize
+ **/
+// organize the state variables into an array:
+public function jsonSerialize() : array {
+	$fields = get_object_vars($this);
+	$fields["profileId"] = $this->profileId->toString();
+	// I had initially included profileActivationToken, but learned that you don't want anything that's a security risk to be included in JSON (like activation token, hash, salt, email). Commenting out to keep for reference.
+	//format the date so that the frontend can consume it
+	// I don't currently have a date attribute relating to the profile entity in my ERD, but I want to keep the following code for reference. 1/23/18
+	//$fields["profileDate"] = round(floatval($this->profileDate->format("U.u")) * 1000);
+	// unset the aspects we don't want to be public
+	unset($fields["profileHash"]);
+	unset($fields["profileSalt"]);
+	return($fields);
 }
